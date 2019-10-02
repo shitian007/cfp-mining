30464c97de365f4b4193fb0b3c4d60dab11dd925
Shi Tian <shitian007@hotmail.com>
2019-10-02 23:32:01 +0800

    Add text checking in link classification

--- cfp_crawl/cfp_spider/spiders/base_conf_spider.py
+++ cfp_crawl/cfp_spider/spiders/base_conf_spider.py
@@ -3,11 +3,12 @@ import urllib
 from urllib.request import Request, urlopen
 import scrapy
 
-from cfp_crawl.classifier import classify_url, URLClass
+from cfp_crawl.classifier import classify_link, URLClass
 from cfp_crawl.config import DOWNLOAD_DELAY, DOWNLOAD_TIMEOUT, LOG_LEVEL, CRAWL_FILEPATH, LOG_FILEPATH, DB_FILEPATH, REQUEST_HEADERS
 from cfp_crawl.cfp_spider.items import ConferenceItem
 from cfp_crawl.cfp_spider.utils import ConferenceHelper
 from cfp_crawl.cfp_spider.wikicfp_parser import WikiConfParser
+
 
 class BaseCfpSpider(scrapy.spiders.CrawlSpider):
 
@@ -19,35 +20,36 @@ class BaseCfpSpider(scrapy.spiders.CrawlSpider):
         'JOBDIR': CRAWL_FILEPATH
     }
 
-
     def parse_conference_page(self, response):
         """
         Parses conference homepages and determines whether found URLs are valid for further crawling
         """
         content_type = response.headers.get('content-type')
         content_type = content_type.decode('utf-8') if content_type else ''
+        if "Errback" in response.request.meta:
+            print("----- From Errback ------")
         if 'application/pdf' in content_type:
-            ConferenceHelper.mark_accessibility(response.url, "Accessible PDF", DB_FILEPATH)
+            ConferenceHelper.mark_accessibility( response.url, "Accessible PDF", DB_FILEPATH)
         else:
             conf_row_id = response.request.meta['row_id']
             # TODO This is different for wayback
             conference_domain = '{url.scheme}://{url.netloc}'.format(url=urllib.parse.urlparse(response.url))
             if "web.archive.org" in conference_domain:
                 return
-            # Possible further crawls from all links on homepage
-            further_crawls = []
-            conference_home_links = response.xpath('//a/@href')
 
+            conference_home_links = response.xpath('//a')
             auxiliary_urls: List[str] = []
             # Classify URL class
-            for link_elem in conference_home_links:
-                link = link_elem.get()
+            for link_selector in conference_home_links:
+                link = link_selector.xpath('@href').get()
+                link = link if link else ""
                 # Rectify partial links
                 full_link = link if bool(urllib.parse.urlparse(link).netloc) else "/".join([conference_domain, link])
-                url_class = classify_url(full_link)
+
+                url_class = classify_link(link_selector)
                 if url_class != URLClass.UNKNOWN:
                     auxiliary_urls.append(full_link)
-                    ConferenceHelper.add_url_db(full_link, conf_row_id, DB_FILEPATH)
+                    ConferenceHelper.add_url_db( full_link, conf_row_id, DB_FILEPATH)
         return
 
 
@@ -62,10 +64,7 @@ class BaseCfpSpider(scrapy.spiders.CrawlSpider):
         row_id = ConferenceHelper.add_conf_db(parsed_conference, DB_FILEPATH)
         url = parsed_conference['url']
         if url:
-            if parsed_conference['wayback_url']:
-                return self.process_conference_url(url, row_id, parsed_conference['wayback_url'])
-            else:
-                return self.process_conference_url(url, row_id)
+            return self.process_conference_url(url, row_id, parsed_conference['wayback_url'])
 
 
     def process_conference_url(self, conf_url: str, row_id: int, wayback_url):
@@ -82,10 +81,11 @@ class BaseCfpSpider(scrapy.spiders.CrawlSpider):
         try:
             conf_link_res = urlopen(Request(conf_url, headers=REQUEST_HEADERS))
             if conf_link_res.status == 200:
-                ConferenceHelper.mark_accessibility(conf_url, "Accessible URL", DB_FILEPATH) # Mark URL accessible
+                ConferenceHelper.mark_accessibility(
+                    conf_url, "Accessible URL", DB_FILEPATH)  # Mark URL accessible
                 return scrapy.spiders.Request(url=conf_url, dont_filter=True, meta=meta,
-                                                callback=self.parse_conference_page,
-                                                errback=self.handle_request_error)
+                                              callback=self.parse_conference_page,
+                                              errback=self.handle_request_error)
         except Exception as e:
             if e.__class__ == urllib.error.HTTPError:
                 ConferenceHelper.mark_accessibility(conf_url, "HTTP Error", DB_FILEPATH)
@@ -95,8 +95,8 @@ class BaseCfpSpider(scrapy.spiders.CrawlSpider):
                 ConferenceHelper.mark_accessibility(conf_url, "Certificate Error", DB_FILEPATH)
             if wayback_url != "Not Available":
                 return scrapy.spiders.Request(url=wayback_url, dont_filter=True, meta=meta,
-                                                callback=self.parse_conference_page,
-                                                errback=self.handle_request_error)
+                                              callback=self.parse_conference_page,
+                                              errback=self.handle_request_error)
 
 
     def handle_request_error(self, err):
@@ -111,7 +111,9 @@ class BaseCfpSpider(scrapy.spiders.CrawlSpider):
         print("wayback: {}".format(err.request.meta['wayback_url']))
         print("===============================================")
         ConferenceHelper.mark_accessibility(err.request.url, "Crawler Access Error", DB_FILEPATH)
-        if "wayback" not in err.request.url:
-            return scrapy.spiders.Request(url=err.request.meta['wayback_url'], callback=self.parse_conference_page,
-                                                errback=self.handle_request_error,
-                                                dont_filter=True)
+        meta = err.request.meta
+        if "wayback" not in err.request.url and "Errback" not in meta:
+            meta['Errback'] = True
+            return scrapy.spiders.Request(url=meta['wayback_url'], dont_filter=True, meta=meta,
+                                          callback=self.parse_conference_page,
+                                          errback=self.handle_request_error)
--- cfp_crawl/classifier.py
+++ cfp_crawl/classifier.py
@@ -14,19 +14,19 @@ org = 'organiz[a-z]+|committee[a-z]*|prog[a-z]*'
 speakers = 'speaker[a-z]*|tutorial|workshop'
 admin = 'date|schedule|loca[a-z]+'
 
-def classify_url(url: str):
+def classify_link(link: 'Selector'):
     """
-    Classifies url
+    Takes a xpath Selector and classifies link based on both URL and text
     """
-    parsed_url: 'ParseResult' = urlparse(url)
-    domain = parsed_url.netloc
-    # Take only path and query, lowercase
-    cleaned_url: str = re.sub(r'html', '', '{url.path}#{url.query}'.format(url=parsed_url)).lower()
-    if re.search(org, cleaned_url):
+    url = link.xpath('@href').get()
+    url = url.lower() if url else ""
+    text = link.xpath('text()').get()
+    text = text.lower() if text else ""
+    if re.search(org, url) or re.search(org, text):
         return URLClass.COMMITTEE
-    elif re.search(speakers, cleaned_url):
+    elif re.search(speakers, url) or re.search(speakers, text):
         return URLClass.SPEAKERS
-    elif re.search(speakers, cleaned_url):
+    elif re.search(admin, url) or re.search(admin, text):
         return URLClass.ADMINISTRATIVE
     else:
         return URLClass.UNKNOWN
