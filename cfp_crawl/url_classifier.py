24c9311e61355996cb436082078a9ae4403e5ca5
Shi Tian <shitian007@hotmail.com>
2019-12-24 22:22:45 +0800

    Change URL classifier relevant link text

--- cfp_crawl/url_classifier.py
+++ cfp_crawl/url_classifier.py
@@ -11,24 +11,15 @@ class URLClass:
 
 
 # Regex string representations of possible keywords
-org = 'organiz[a-z]+|committee[a-z]*|prog[a-z]*'
+org = 'organiz[a-z]+|committee[a-z]*|prog[a-z]*|chair'
 speakers = 'author[s]*|speaker[a-z]*|tutorial|workshop'
-admin = 'date|schedule|loca[a-z]+|regist[a-z]+'
 
-
-def classify_link(link: 'Selector'):
-    """
-    Takes a xpath Selector and classifies link based on both URL and text
-    """
-    url = link.xpath('@href').get()
-    url = url.lower() if url else ""
-    text = link.xpath('text()').get()
-    text = text.lower() if text else ""
+def classify_link(link_text, link_url):
+    text = link_text.lower() if link_text else ""
+    url = link_url.lower() if link_url else ""
     if re.search(org, url) or re.search(org, text):
         return URLClass.COMMITTEE
     elif re.search(speakers, url) or re.search(speakers, text):
         return URLClass.SPEAKERS
-    elif re.search(admin, url) or re.search(admin, text):
-        return URLClass.ADMINISTRATIVE
     else:
         return URLClass.UNKNOWN
