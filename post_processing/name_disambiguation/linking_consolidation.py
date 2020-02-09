97a8d604c71bfbac8971055829d498a4b2f08276
Shi Tian <shitian007@hotmail.com>
2020-02-09 16:49:29 +0800

    Add retrieval of external API ids

--- post_processing/name_disambiguation/linking_consolidation.py
+++ post_processing/name_disambiguation/linking_consolidation.py
@@ -1,8 +1,9 @@
 import argparse
 import pickle
 import sqlite3
+from external_api import API
 from tfidf_clustering import Clustering
-from nd_utils import clean_punctuation, create_tables
+from nd_utils import clean_punctuation, DatabaseHelper
 
 
 class Consolidator:
@@ -11,6 +12,7 @@ class Consolidator:
         self.original_db_cnx = original_db_cnx
         self.consolidated_db_cnx = consolidated_db_cnx
         self.clustering = clustering
+        self.api = API()
 
     def org_cluster_rep(self, ent: str):
         """ Retrieves the cluster representative of the given organization
@@ -43,10 +45,10 @@ class Consolidator:
         original_db_cur = self.original_db_cnx.cursor()
         consolidated_db_cur = self.consolidated_db_cnx.cursor()
 
-        conferences = original_db_cur.execute(
+        conference_ids = original_db_cur.execute(
             "SELECT id FROM WikicfpConferences ORDER BY id").fetchall()
 
-        for conf_id in conferences:
+        for conf_id in conference_ids:
             conf_id = conf_id[0]
             # Copy over table of conferences and conference pages
             conf = original_db_cur.execute(
@@ -83,32 +85,58 @@ class Consolidator:
                 else:
                     person_id = person_id[0]
                 consolidated_db_cur.execute("INSERT INTO PersonRole (role_type, conf_id, person_id) VALUES (?, ?, ?)",
-                                           (role, conf_id, person_id))
+                                            (role, conf_id, person_id))
 
         self.consolidated_db_cnx.commit()
 
+    def retrieve_external_ids(self, num_to_search, similarity_threshold):
+        consolidated_db_cur = self.consolidated_db_cnx
+        person_ids = consolidated_db_cur.execute(
+            "SELECT id FROM Persons ORDER BY id").fetchall()
+        for person_id in person_ids[74:]:
+            person_id = person_id[0]
+            person_name, org = consolidated_db_cur.execute("SELECT p.name, o.name FROM Persons p\
+                JOIN Organizations o ON p.org_id=o.id WHERE p.id=?", (person_id,)).fetchone()
+            retrieved_persons: 'List[Person]' = self.api.get_person_results(
+                person_name, org, num_to_search)
+            # Filter retrieved persons with low similarity
+            retrieved_persons = list(filter(
+                lambda p: self.api.similarity(
+                    p.name, person_name) < similarity_threshold,
+                retrieved_persons))
+            for person in retrieved_persons:
+                existing_id = consolidated_db_cur.execute(
+                    "SELECT {} FROM Persons WHERE id=?".format(person.type), (person_id,)).fetchone()
+                if not existing_id:
+                    consolidated_db_cur.execute(
+                        "UPDATE Persons SET {}=? WHERE id=?".format(person.type), (person.id, person_id))
+            self.consolidated_db_cnx.commit()
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('clustering_filepath', type=str,
-                    help="Pickled Clustering")
-parser.add_argument('original_db_filepath', type=str,
-                    help="Database file to disambiguate (No alteration to this db)")
-parser.add_argument('consolidated_db_filepath', type=str,
-                    help="New consolidated database file save location")
-args = parser.parse_args()
 
-# Database connections and Clustering object
-original_db_cnx = sqlite3.connect(args.original_db_filepath)
-consolidated_db_cnx = sqlite3.connect(args.consolidated_db_filepath)
-with open(args.clustering_filepath, 'rb') as cluster_file:
-    clustering = pickle.load(cluster_file)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('clustering_filepath', type=str,
+                        help="Pickled Clustering")
+    parser.add_argument('original_db_filepath', type=str,
+                        help="Database file to disambiguate (No alteration to this db)")
+    parser.add_argument('consolidated_db_filepath', type=str,
+                        help="New consolidated database file save location")
+    args = parser.parse_args()
 
-# Create necessary tables for consolidated data
-create_tables(consolidated_db_cnx)
-# Actual consolidation after disambiguation
-consolidator = Consolidator(original_db_cnx, consolidated_db_cnx, clustering)
-consolidator.process()
+    # Database connections and Clustering object
+    original_db_cnx = sqlite3.connect(args.original_db_filepath)
+    consolidated_db_cnx = sqlite3.connect(args.consolidated_db_filepath)
+    with open(args.clustering_filepath, 'rb') as cluster_file:
+        clustering = pickle.load(cluster_file)
 
-# Close connections
-original_db_cnx.close()
-consolidated_db_cnx.close()
+    # Create necessary tables for consolidated data
+    DatabaseHelper.create_tables(consolidated_db_cnx)
+    # Actual consolidation after disambiguation
+    consolidator = Consolidator(
+        original_db_cnx, consolidated_db_cnx, clustering)
+    # consolidator.process()
+    consolidator.retrieve_external_ids(5, 3)
+
+    # Close connections
+    original_db_cnx.close()
+    consolidated_db_cnx.close()
